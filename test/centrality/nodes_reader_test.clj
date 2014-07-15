dcfca2a77f99149e067501f7ad915ceb473e8c53
Sergio Azevedo <sergior.jr@gmail.com>
2014-07-15 00:43:16 -0300

    adds hashMap-representation test

--- test/centrality/nodes_reader_test.clj
+++ test/centrality/nodes_reader_test.clj
@@ -4,12 +4,23 @@
 )
 
 (deftest nodes-reader-test
-  (testing "build a graph representation from a array of arrays"
+  (testing "build-graph returns a HashMap instance"
     (let [
         nodes [ ["A" "B"] ["B" "C"] ["B" "D"] ["A" "E"] ]
         graph (build-graph [nodes])
       ]
       (is (instance? clojure.lang.PersistentHashMap graph))
+    )
+  )
+)
+
+(deftest hasMap-representation
+  (testing "build a graph representation from a array of arrays"
+    (let [
+        nodes [ ["A" "B"] ["B" "C"] ["B" "D"] ["A" "E"] ]
+        graph (build-graph [nodes])
+      ]
+      (is (= {:A ["B" "E"] :B ["C" "D"] :C ["B"] :D ["B"] :E ["A"]} graph))
     )
   )
 )
