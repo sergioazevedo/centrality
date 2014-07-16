504b9900b36bf8ae610c11b34b321dc4ddd2edc6
Sergio Azevedo <sergior.jr@gmail.com>
2014-07-16 00:26:53 -0300

    change implementation of add-node to support the test add-1-new-node-to-non-empty-graph

--- src/centrality/nodes_reader.clj
+++ src/centrality/nodes_reader.clj
@@ -17,12 +17,40 @@
   (get array 1)
   )
 
+(defn- exist-key-in-graph
+  [key graph]
+  (contains? graph key)
+  )
+
+(defn- add-new-node-into-graph
+  [new-node graph]
+  (let[
+    key (extract-first-element-from-as-keyword new-node)
+    value (extract-last-element-from new-node)]
+
+    (assoc graph key [value])
+    )
+  )
+
+(defn- append-new-value-to-graph-key
+  [new-node graph]
+  (let[
+    key (extract-first-element-from-as-keyword new-node)
+    values (get graph key )
+    new-value (extract-last-element-from new-node)]
+
+    (assoc graph key (conj values new-value))
+    )
+  )
+
 (defn add-node
   [new-node graph]
   (let[
-    value (extract-last-element-from new-node)
-    result {(keyword key) [value]}
     key (extract-first-element-from-as-keyword new-node)
+    key-exists? (exist-key-in-graph key graph)
     ]
-    result
+    (if key-exists?
+      (append-new-value-to-graph-key new-node graph)
+      (add-new-node-into-graph new-node graph)
+      )
   ))
